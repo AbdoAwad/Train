4849be885e12e96955726ccd2474b5a71a597320
AbdoAwad <ar.awad264@gmail.com>
2019-02-20 13:28:20 +0800

    A. Dice Rolling

---
+++ DiceRolling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a;
+    for (int i = 0; i < n; ++i)
+    {
+        
+        cin >> a;
+        cout << (a/7) + 1 << endl;
+    }
+}
