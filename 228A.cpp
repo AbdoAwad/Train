7466f740953c5ce7ab7cc95556647cd962f26967
AbdoAwad <ar.awad264@gmail.com>
2019-03-25 23:34:46 +0800

    Problem A - Is your horseshoe on the other hoof?

---
+++ 228A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[4];
+    int cnt = 0;
+    for (int i = 0; i < 4; ++ i)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 3; ++ i)
+    {
+        for (int j = i + 1; j < 4; ++ j)
+        {
+            if (arr[i] == arr[j] )
+            {
+                ++cnt;
+                break;
+            }
+        }
+    }
+    cout << cnt << endl;
+}
