94f2dc61f4cb7bee48c7a02bee65f7058d5268ef
AbdoAwad <ar.awad264@gmail.com>
2019-03-29 19:02:37 +0800

    A - The number of positions

---
+++ 124A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    int cnt = 0;
+
+    for (int i = 0; i <= b; ++i)
+    {
+        if (i + 1 + a <= n)
+            ++cnt;
+    }
+    cout << cnt << endl;
+    
+}
